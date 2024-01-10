9e0ca5eac0927333e342ef7b81006c5e84003536
Rocco Iuliano <rocco.iul2000@gmail.com>
2024-01-10 20:55:59 +0100

    Developed method to test model and to compute metrics

--- train.py
+++ train.py
@@ -102,6 +102,113 @@ def run_validation(model, validation_ds, tokenizer_src, tokenizer_tgt, max_len, 
         print_msg('-'*console_width)
 
 
+def run_test(model, test_ds, tokenizer_src, tokenizer_tgt, max_len, device, print_msg, writer):
+    model.eval()
+    count = 0
+
+    source_texts = []
+    expected = []
+    predicted = []
+
+    try:
+        # get the console window width
+        with os.popen('stty size', 'r') as console:
+            _, console_width = console.read().split()
+            console_width = int(console_width)
+    except:
+        # If we can't get the console width, use 80 as default
+        console_width = 80
+
+    with torch.no_grad():
+        i=1
+        batch_iterator = tqdm(test_ds, desc=f"Processing Sentence {i:02d}")
+        for batch in batch_iterator:
+            count += 1
+            encoder_input = batch["encoder_input"].to(device) # (b, seq_len)
+            encoder_mask = batch["encoder_mask"].to(device) # (b, 1, 1, seq_len)
+
+            # check that the batch size is 1
+            assert encoder_input.size(0) == 1, "Batch size must be 1 for test"
+
+            model_out = greedy_decode(model, encoder_input, encoder_mask, tokenizer_src, tokenizer_tgt, max_len, device)
+
+            source_text = batch["src_text"][0]
+            target_text = batch["tgt_text"][0]
+            model_out_text = tokenizer_tgt.decode(model_out.detach().cpu().numpy())
+
+            source_texts.append(source_text)
+            expected.append(target_text)
+            predicted.append(model_out_text)
+            if i % 20==0:
+                str_1=' Prediction '+i+' '
+                new_console_width=int((console_width-len(str_1))/2)
+                str_2='-'*new_console_width
+                print_msg(str_2+str_1+str_2)
+                print_msg(f"{f'SOURCE: ':>12}{source_text}")
+                print_msg(f"{f'TARGET: ':>12}{target_text}")
+                print_msg(f"{f'PREDICTED: ':>12}{model_out_text}")
+                print_msg('-'*console_width)
+            i+=1
+    
+    if writer:
+        cer,wer,bleu,avg_meteor=compute_metrics(writer, tokenizer_tgt, predicted, expected, test=True)
+        str_1=' Test evaluation metrics '
+        new_console_width=int((console_width-len(str_1))/2)
+        str_2='-'*new_console_width
+        print_msg(str_2+str_1+str_2)
+        print_msg(f"{f'Char Error Rate: ':>12}{cer}")
+        print_msg(f"{f'Word Error Rate: ':>12}{wer}")
+        print_msg(f"{f'BLEU: ':>12}{bleu}")
+        print_msg(f"{f'METEOR: ':>12}{avg_meteor}")
+        print_msg('-'*console_width)
+
+
+def compute_metrics(writer, tokenizer_tgt, predicted, expected, global_step, test:bool=False):
+    # Evaluate the character error rate
+    # Compute the char error rate 
+    metric = torchmetrics.CharErrorRate()
+    cer = metric(predicted, expected)
+    if test:
+        writer.add_scalar('test cer', cer)
+    else:
+        writer.add_scalar('validation cer', cer, global_step)
+    writer.flush()
+
+    # Compute the word error rate
+    metric = torchmetrics.WordErrorRate()
+    wer = metric(predicted, expected)
+    if test:
+        writer.add_scalar('test wer', wer)
+    else:
+        writer.add_scalar('validation wer', wer, global_step)
+    writer.flush()
+
+    # Compute the BLEU metric
+    metric = torchmetrics.BLEUScore()
+    bleu = metric(predicted, expected)
+    if test:
+        writer.add_scalar('test ', bleu)
+    else:
+        writer.add_scalar('validation BLEU', bleu, global_step)
+    writer.flush()
+
+    # Compute the METEOR metric
+    meteor_scores=list()
+    for exepectation, prediction in zip(expected, predicted):
+        expected_tokens = tokenizer_tgt.encode(exepectation).ids
+        predicted_tokens = tokenizer_tgt.encode(prediction).ids 
+        score = meteor_score.single_meteor_score(expected_tokens, predicted_tokens)
+        meteor_scores.append(score)
+    average_meteor_score= sum(meteor_scores) / len(meteor_scores)
+
+    if test:
+        writer.add_scalar('average test METEOR', average_meteor_score)
+    else:
+        writer.add_scalar('average validation METEOR', average_meteor_score, global_step)
+    writer.flush()
+
+    return cer, wer, bleu, average_meteor_score
+
 def get_model(config, vocab_src_len, vocab_tgt_len):
     model = build_transformer(vocab_src_len, vocab_tgt_len, config["seq_len"], config['seq_len'], d_h=config['d_h'])
     return model
@@ -209,3 +316,15 @@ def train_and_test_model(config):
     print("------------------------------------------------ START TEST ------------------------------------------------")
     run_test(model, test_dataloader, tokenizer_src, tokenizer_tgt, config['seq_len'], device, lambda msg: batch_iterator.write(msg), writer)
     
+
+
+if __name__ == '__main__':
+    warnings.filterwarnings("ignore")
+    source_lang=str(input("Insert the source language (de, es, fr, it, nl, pl, pt, ru):\n"))
+    model_folder=str(input("Insert model folder name (model_srclang_tgtlang):\n"))
+    experiment_name=str(input("Insert the experiment name:\n"))
+    source_lang=source_lang.strip()
+    model_folder=model_folder.strip()
+    experiment_name=experiment_name.strip()
+    config = get_config(model_folder, source_lang, experiment_name)
+    train_and_test_model(config)
