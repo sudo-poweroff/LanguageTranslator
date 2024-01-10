82d60f4b7091fb5ac7eeedad8415d09dee37e464
Rocco Iuliano <rocco.iul2000@gmail.com>
2024-01-10 20:45:01 +0100

    Developed build transformer function

--- model.py
+++ model.py
@@ -58,5 +58,45 @@ class Transformer(nn.Module):
         # (batch, seq_len, vocab_size)
         return self.projection_layer(x)
     
+def build_transformer(src_vocab_size: int, tgt_vocab_size: int, src_seq_len: int, tgt_seq_len: int, d_h: int=512, N: int=6, h: int=8, dropout: float=0.1, d_ff: int=2048) -> Transformer:
+    # Create the embedding layers
+    src_embed = InputEmbeddings(d_h, src_vocab_size)
+    tgt_embed = InputEmbeddings(d_h, tgt_vocab_size)
 
+    # Create the positional encoding layers
+    src_pos = PositionalEncoding(d_h, src_seq_len, dropout)
+    tgt_pos = PositionalEncoding(d_h, tgt_seq_len, dropout)
+    
+    # Create the encoder blocks
+    encoder_blocks = []
+    for _ in range(N):
+        encoder_self_attention_block = MultiHeadAttentionBlock(d_h, h, dropout)
+        feed_forward_block = FeedForwardBlock(d_h, d_ff, dropout)
+        encoder_block = EncoderBlock(d_h, encoder_self_attention_block, feed_forward_block, dropout)
+        encoder_blocks.append(encoder_block)
 
+    # Create the decoder blocks
+    decoder_blocks = []
+    for _ in range(N):
+        decoder_self_attention_block = MultiHeadAttentionBlock(d_h, h, dropout)
+        decoder_cross_attention_block = MultiHeadAttentionBlock(d_h, h, dropout)
+        feed_forward_block = FeedForwardBlock(d_h, d_ff, dropout)
+        decoder_block = DecoderBlock(d_h, decoder_self_attention_block, decoder_cross_attention_block, feed_forward_block, dropout)
+        decoder_blocks.append(decoder_block)
+    
+    # Create the encoder and decoder
+    encoder = Encoder(d_h, nn.ModuleList(encoder_blocks))
+    decoder = Decoder(d_h, nn.ModuleList(decoder_blocks))
+    
+    # Create the projection layer
+    projection_layer = ProjectionLayer(d_h, tgt_vocab_size)
+    
+    # Create the transformer
+    transformer = Transformer(encoder, decoder, src_embed, tgt_embed, src_pos, tgt_pos, projection_layer)
+    
+    # Initialize the parameters with Xavier technique
+    for p in transformer.parameters():
+        if p.dim() > 1:
+            nn.init.xavier_uniform_(p) 
+    
+    return transformer
